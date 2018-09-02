7564fa1ab7fbdecb045a88f23ed3f10eab6e4131
Kurt <kurt@FuBar.local>
2018-09-02 19:31:52 -0400

    install react-Semantic-UI

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import HeaderImgContainer from './HeaderImgContainer'
+import '../stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeaderImgContainer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/HeaderImgContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HeaderImgContainer extends Component {
+    render() {
+        return (
+            <div>
+                whoops!
+            </div>
+        );
+    }
+}
+
+export default HeaderImgContainer;
