050e6a7e63153fc5f087e8d1b6ee60037cbe360c
Kurt <kurt@FuBar.local>
2018-09-05 17:37:46 -0400

    add button that calles backend API, in landingpagecontainer component, to remove later

--- src/components/LandingPageContainer.js
+++ src/components/LandingPageContainer.js
@@ -5,11 +5,16 @@ import Blurb from './Blurb'
 import LoginButtons from './LoginButtons'
 
 class LandingPageContainer extends Component {
+    doThis = () => {
+        window.location = 'http://localhost:3001/api/v1/fetchbills'
+    }
+
     render() {
         return (
             <React.Fragment>
                 <Blurb />
                 <LoginButtons/>
+                <Button onClick={this.doThis} >Fetch Bills</Button>
             </React.Fragment>
         );
     }
