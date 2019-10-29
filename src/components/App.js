8b15633b7d1b9da3a4d1c8494b7e75dc9035ee15
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-29 18:35:31 -0400

    change path for landingPage now in common folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {connect} from 'react-redux'
-import LandingPageContainer from './LandingPageContainer'
+import LandingPage from '../common/LandingPage'
 import SignUp from './SignUp'
 import LogIn from './LogIn'
 import SearchBills from './SearchBills'
@@ -175,7 +175,7 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={LandingPageContainer} />
+        <Route exact path="/" component={LandingPage} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={LogIn} />
         <Route path="/searchbills" render={(renderProps)=>(
