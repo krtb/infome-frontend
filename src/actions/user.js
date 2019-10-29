059bb1ffeac1a6c994a2dc3bd96c6b6f3cdeba82
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-29 17:09:08 -0400

    clean up syntax

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,44 +1,47 @@
-import { isFlowDeclaration } from "@babel/types"
 
 export const loginUser = (name, password) => {
+
     return dispatch => {
         dispatch(authenticatingUser())
-        //let dev_api_url = 'http://localhost:3001/api/v1/login' ---> remove dev api url, for prod '/resources'
-         let prod_api ='https://infome-backend.herokuapp.com/api/v1/login'
-        fetch(prod_api , {
+        const prod_login_endpoint = 'https://infome-backend.herokuapp.com/api/v1/login'
+        //const dev_endpoint= 'http://localhost:3001/api/v1/login' ---> remove dev api url, for prod '/resources'
+        const postFetchObject = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
             },
             body: JSON.stringify({ user: { name, password } })
-            // altered npm pacakage read/write 
+        }
+
+        fetch(prod_login_endpoint, postFetchObject)
+        .then(response => response.json())
+        .then(({ user, jwt }) => {
+            localStorage.setItem('jwt', jwt)
+            dispatch(setCurrentUser(user))
         })
-            .then(response => response.json())
-            .then(({ user, jwt }) => {
-                localStorage.setItem('jwt', jwt)
-                dispatch(setCurrentUser(user))
-            })
-            .catch(error =>
-                console.log(error.message, 'ERROR HERE!') //fix catch syntax
-            )
+        .catch(error =>
+            console.log(error.message, 'ERROR HERE!') //fix catch syntax
+        )
     }
+
 }
 
 export const fetchCurrentUser = () => {
     // takes the token in localStorage and finds out who it belongs to
     return dispatch => {
-        let dev_api_url = 'http://localhost:3001/api/v1/profile' //---> remove dev api url, for prod '/resources'
-        //let prod_api = 'https://infome-backend.herokuapp.com/api/v1/profile'
-
-        fetch(dev_api_url, {
+        const dev_profile_endpoint = 'http://localhost:3001/api/v1/profile' //---> remove dev api url, for prod '/resources'
+        //const prod_profile_endpoint = 'https://infome-backend.herokuapp.com/api/v1/profile' TODO: add this to .env file
+        const getFetchObject = {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
             }
-        })
-            .then(response => response.json())
-            .then(({ user }) => dispatch(setCurrentUser(user)))
+        }
+
+        fetch(dev_profile_endpoint, getFetchObject)
+        .then(response => response.json())
+        .then(({ user }) => dispatch(setCurrentUser(user)))
     }
 }
 
