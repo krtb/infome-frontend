02e8c5ff0d7ca3a32c9ec93c6ebc115cdcd59205
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-29 18:49:36 -0400

    remove components directory from file path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
-import App from './components/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, withRouter} from 'react-router-dom';
