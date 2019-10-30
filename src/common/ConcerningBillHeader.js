9a7cb2130bc2d38b348a29ee8ac9a03885c48da0
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-30 10:01:22 -0400

    add files to tracking now that they've been modified

---
+++ src/common/ConcerningBillHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header, Icon, Image } from 'semantic-ui-react'
+
+const ConcerningBillHeader = () => (
+    <div>
+        <Header as='h2' icon textAlign='center'>
+            <i id="smiley" className="far fa-dizzy"></i>
+            <Header.Content>Concerning Bills</Header.Content>
+        </Header>
+    </div>
+)
+
+export default ConcerningBillHeader
---
+++ src/common/ProductiveBillHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header, Icon, Image } from 'semantic-ui-react'
+
+const ProductiveBillHeader = () => (
+    <div>
+        <Header as='h2' icon textAlign='center'>
+            <i id="smiley" className="far fa-grin-beam"></i>
+            <Header.Content>Productive Bills</Header.Content>
+        </Header>
+    </div>
+)
+
+export default ProductiveBillHeader
