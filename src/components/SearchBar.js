e09c791f0db9627d8e5edb7069861ace51fbe27e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-30 10:34:31 -0400

    move these 2 files into search component folder

--- src/components/SearchBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
-
-const SearchBar = (props) => (
-    <Input value={props.searchTerm} onChange={(event)=>props.handleSearch(event)} action={{color: 'teal'}} label={{ icon: 'asterisk' }} labelPosition='right corner' placeholder='Search...' />
-)
-
-export default SearchBar
