456ae711e5ae8e02ed06c54cbbce92b6589cded0
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-30 10:00:41 -0400

    add common folder to file path

--- src/components/SavedBills.js
+++ src/components/SavedBills.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Table } from 'semantic-ui-react'
 import MenuTabs from '../common/MenuTabs'
-import ProductiveBillHeader from './ProductiveBillHeader';
-import ConcerningBillHeader from './ConcerningBillHeader';
+import ProductiveBillHeader from '../common/ProductiveBillHeader';
+import ConcerningBillHeader from '../common/ConcerningBillHeader';
 import SavedBillsTable from './SavedBillsTable';
 
 import SavedNegativeBillsTable from './SavedNegativeBillsTable'
