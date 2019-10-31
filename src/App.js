9fbb06ad82695d9c5b876f3d5ef71ff9cce9c3d2
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-31 17:21:50 -0400

    consolidate functions adding bills to concerningBills amd productiveBills array

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LandingPage from './components/common/LandingPage'
 import CreateAccountForm from './components/session/CreateAccountForm'
 import LoginForm from './components/session/LoginForm'
 import SearchBills from './components/search/SearchBills'
-import SavedBills from './components/bill/SavedBills'
+import SavedBillsTable from './components/bill/SavedBillsTable'
 import UserProfile from './components/session/UserProfile'
 import usersReducer from './reducers/usersReducer';
 import withAuth from './hocs/withAuth'
@@ -14,106 +14,19 @@ const token = localStorage.getItem('jwt')
 
 
 class App extends Component {
-
-
-  // componentDidMount() {
-  //   this.fetchAllBillsInitially()
-  // }
-
-  // fetchAllBillsInitially = () => {
-  //   const token = localStorage.getItem('jwt')
-
-  //   if (token) {
-  //     const fetchObject = {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'Application/json',
-  //         'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-  //       }
-  //     }
-  //     fetch('http://localhost:3001/api/v1/fetchbills', fetchObject).then(resp => resp.json()).then(data => {
-  //       this.setState({
-  //         changing_upcoming_bill_data: data.results[0].bills
-  //       })
-  //     })
-  //   }
-  // }
-
-  // testFetch = () => {
-
-  // }
-
-  // componentDidMount() {
-  //   this.fetchAllBillsInitially()
-  //  }
-
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   if(nextState.upcoming_bill_data.length !== 0){
-  //     return false
-  //   } else {
-  //     return true
-  //   }
-  // }
-  
-  // componentDidUpdate() {
-  //   console.log("true!");
-  //   this.fetchData()
-  // }
-
-
-  // ---------
-
   state = {
     upcoming_bill_data: [],
     changing_upcoming_bill_data: [],
     isClicked: false,
+    isProductive: "isProductive",
+    isConcerning: "isConcerning",
     productiveBills: [],
     concerningBills: [],
     searchTerm: ''
   }
 
-  //   if (token) {
-  //     const UPCOMING_BILLS_API = 'http://localhost:3001/api/v1/bills'
-  //     const fetchObject = {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'Application/json',
-  //         'Authorization': `Bearer ${token}`
-  //       }
-  //     }
-
-  //     return fetch(UPCOMING_BILLS_API, fetchObject).then(resp => resp.json()).then(data => this.setState({
-  //       upcoming_bill_data: data.bills,
-  //       changing_upcoming_bill_data: data.bills
-  //     }))
-  //   }
-  // }
-
-
-  handleBillClick = (bill) => {
-    console.log(bill);
-    let my_bill = bill.bill_id
-
-    if (!this.state.productiveBills.includes(bill)) {
-      this.setState({
-        productiveBills: [...this.state.productiveBills, bill]
-      })
-    }
-    this.fetchPostBill(my_bill)
-  }
-
-  handleNegativeBillClick = (bill) => {
-    // console.log(bill);
-    if (!this.state.concerningBills.includes(bill)) {
-      this.setState({
-        concerningBills: [...this.state.concerningBills, bill]
-      })
-    }
-  }
-
   fetchPostBill = (bill_id) => {
-    // const USER_API = 'http://localhost:3001/api/v1/user_bills' ---> remove hard coded path, WORK WITH HEROKU
-    let prod_api ='https://infome-backend.herokuapp.com/api/v1/user_bills'
+    let prod_api = 'https://infome-backend.herokuapp.com/api/v1/user_bills'
     const fetchObj = {
       method: 'POST',
       headers: {
@@ -122,55 +35,31 @@ class App extends Component {
       },
       body: JSON.stringify({
         bill_id: bill_id
-        // user_id: this.props.user_id  
-      }) 
+      })
     }
-    return fetch(prod_api , fetchObj).then(resp=>resp.json()) // ---> removed hard coded path
+    return fetch(prod_api, fetchObj).then(resp => resp.json()) // ---> removed hard coded path
   } 
 
+  handleBillChoiceClick = (bill, choice) => {
 
-  // ------
+    let my_bill = bill.bill_id
+    this.fetchPostBill(my_bill)
 
-  // frontEndDeleteButton(BillId) {
-  //   console.log(BillId)
-  //   let bill_number = BillId
-  //   console.log(bill_number)
-  //   let divOne = document.getElementById(bill_number);
-  //   console.log(divOne);
+    if(!this.state.productiveBills.includes(bill) && choice == "isProductive"){
+      this.setState({
+        productiveBills: [...this.state.productiveBills, bill]
+      })
+      console.log(this.state.productiveBills)
+    } 
     
-  //   // divOne.style.visibility= 'hidden';
-  // }
+    if(!this.state.concerningBills.includes(bill) && choice == "isConcerning") {
+      this.setState({
+        concerningBills: [...this.state.concerningBills, bill]
+      })
+      console.log(this.state.concerningBills)
 
-  // frontEndPosDeleteButton(item) {
-  //   this.setState(prevState => {
-  //     return {
-  //       product: {
-  //         ...productiveBills,
-  //         ingredients: productiveBills.filter((bill) => ingredient !== item)
-  //       }
-  //     }
-  //   });
-  // }
-
-  // handleSearch = (event) => {
-  //   console.log('handleSearch', event)
-  //   this.setState({
-  //     searchTerm: event.target.value
-  //   }, () => this.filterSearch())
-  // }
-
-  //   filterSearch = () => {
-  //     console.log('filterSearch')
-  //   let findBill = this.state.upcoming_bill_data.filter((aBill) => (
-  //     aBill.description.toLowerCase().includes(this.state.searchTerm.toLowerCase())
-  //     )
-  //   )
-
-  //   this.setState({
-  //     changing_upcoming_bill_data: findBill 
-  //   })
-  // }
-
+    }
+  }
 
   render() {
     return (
@@ -179,28 +68,34 @@ class App extends Component {
         <Route path="/CreateAccountForm" component={CreateAccountForm} />
         <Route path="/login" component={LoginForm} />
         <Route path="/searchbills" render={(renderProps)=>(
+
           <SearchBills {...renderProps} 
           changeUpcomBilDat={this.state.changing_upcoming_bill_data}
           addToUser={this.handleBillClick}
           addNegaToUser={this.handleNegativeBillClick}
-          handleSearch={this.handleSearch} />
+          handleSearch={this.handleSearch} 
+          handleBillChoiceClick={this.handleBillChoiceClick}
+          isProductive={this.state.isProductive}
+          isConcerning={this.state.isConcerning}
+          />
+
         )} />
         <Route path="/savedbills" render={(renderProps)=>(
-          <SavedBills {...renderProps}
+
+          <SavedBillsTable {...renderProps}
+            handleBillChoiceClick={this.handleBillChoiceClick}
+            isProductive={this.state.isProductive}
+            isConcerning={this.state.isConcerning}
             productiveBills={this.state.productiveBills}
             concerningBills={this.state.concerningBills}
             frontEndDeleteButton={this.frontEndDeleteButton}
           />
+
         )} />
         <Route path="/myprofile" component={UserProfile} />
       </Switch>
     );
   }
 }
-// const mapStateToProps = (state) => {
-//   return {
-//     user_id: state.usersReducer.user.id
-//   }
-// }
+
 export default App;
-// export default App;
