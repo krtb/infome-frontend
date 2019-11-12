42dbe2ffbe09e07297b406f8028b63c6721c4e70
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 11:42:28 -0500

    remove store from index, move to configureStore file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-
-import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-
-import App from './App';
-
 import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './reducers/index.js'
-import usersReducer from './reducers/usersReducer'
 import withAuth from './hocs/withAuth'
 
+import App from './App';
 import './stylesheets/index.css';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
+// import * as reducers from './components'
+// import rootReducer from './reducers/index.js'
+// import usersReducer from './reducers/usersReducer'
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+import configureStore from './store/configureStore';
+const store = configureStore()
 const AuthedApp = withAuth(App);
 
 ReactDOM.render(
