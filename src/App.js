4e2cb2aade884801fbe2c11342fe22b17e1ea79b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 13:28:28 -0500

    move index.css out of /stylesheets and delete folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {connect} from 'react-redux'
 import withAuth from './hocs/withAuth'
 
-
 import LandingPage from './components/common/LandingPage'
 import CreateAccountForm from './components/session/CreateAccountForm'
 import LoginForm from './components/session/LoginForm'
