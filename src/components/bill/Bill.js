7a653c524c5f15593c0dae04d1e8cb0a2ad2a7df
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 16:51:35 -0500

    remove unused import

--- src/components/bill/Bill.js
+++ src/components/bill/Bill.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Table, Button, Checkbox, Icon } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-
+import { Table, Button} from 'semantic-ui-react'
 
 const Bill = (props) => {
     const {isConcerning, isProductive, handleBillChoiceClick } = props;
