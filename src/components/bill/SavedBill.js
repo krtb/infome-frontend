4e3437347bbbc5c86220e7d0cd6f8df2ed84567a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 17:24:39 -0500

    removed unused import

--- src/components/bill/SavedBill.js
+++ src/components/bill/SavedBill.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Table, Button, Checkbox, Icon } from 'semantic-ui-react'
 
+
 const SavedBill = (props) => {
     return (
         <Table.Row>
             <Table.Cell>{props.one.bill_number}</Table.Cell>
             <Table.Cell >{props.one.description}</Table.Cell>
             <Table.Cell >{props.one.chamber}</Table.Cell>
-            <Table.Cell selectable><a target="_blank" href={props.one.bill_url}><i className="far fa-file-pdf"></i></a></Table.Cell>
+            <Table.Cell selectable><a target="_blank" rel="noopener noreferrer" href={props.one.bill_url}><i className="far fa-file-pdf"></i></a></Table.Cell>
             <Table.Cell >{props.one.legislative_day}</Table.Cell>
             <Table.Cell selectable>
-                <a><i className="fas fa-skull"></i></a>
+                <a href={props.one.bill_url} ><i className="fas fa-skull"></i></a>
             </Table.Cell>
         </Table.Row>
     );
