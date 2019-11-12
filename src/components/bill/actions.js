db318945de8e1824b4340e178d6383cc6f6b6655
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 11:53:03 -0500

    add searchTerm action that sets bills based on search input

--- src/components/bill/actions.js
+++ src/components/bill/actions.js
@@ -1,9 +1,16 @@
 import getAllBills from '../apis/infomeAPI'
-import FETCH_BILLS from './types'
+import { FETCH_BILLS } from './types'
+import { SEARCH } from '../search/types';
 
-export const fetchPosts = () => async dispatch => {
-
+export const fetchBills = () => async dispatch => {
     const response = await getAllBills.get('/fetchbills');
+    dispatch({ type: FETCH_BILLS, payload: response.data.results[0].bills})
+};
 
-    dispatch({ type: FETCH_BILLS, payload: response })
+export const searchTerm = searchTerm => dispatch => {
+    searchTerm.preventDefault()
+    dispatch({
+        type: SEARCH,
+        payload: searchTerm.target.value
+    });
 };
