684da7160eb4a7c6b3f38508ba9802a64a31d888
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 16:48:34 -0500

    remove unused import, href fix for warning

--- src/components/bill/SavedBill.js
+++ src/components/bill/SavedBill.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Table, Button, Checkbox, Icon } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-
+import { Table} from 'semantic-ui-react'
 
 const SavedBill = (props) => {
     return (
@@ -10,10 +7,10 @@ const SavedBill = (props) => {
             <Table.Cell>{props.one.bill_number}</Table.Cell>
             <Table.Cell >{props.one.description}</Table.Cell>
             <Table.Cell >{props.one.chamber}</Table.Cell>
-            <Table.Cell selectable><a target="_blank" href={props.one.bill_url}><i className="far fa-file-pdf"></i></a></Table.Cell>
+            <Table.Cell selectable><a target="_blank" rel="noopener noreferrer" href={props.one.bill_url}><i className="far fa-file-pdf"></i></a></Table.Cell>
             <Table.Cell >{props.one.legislative_day}</Table.Cell>
             <Table.Cell selectable>
-                <a><i className="fas fa-skull"></i></a>
+                <a href={props.one.bill_url} ><i className="fas fa-skull"></i></a>
             </Table.Cell>
         </Table.Row>
     );
