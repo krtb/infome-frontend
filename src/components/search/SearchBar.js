a20aa359ac190749af6700d7a77597a17f1ec90d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 11:59:49 -0500

    make SearchBar a class component

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Input } from 'semantic-ui-react'
 
-const SearchBar = (props) => (
-    <Input 
-        value={props.searchTerm} 
-        onChange={(event)=>props.handleSearch(event)} 
-        action={{color: 'teal'}} label={{ icon: 'asterisk' }} 
-        labelPosition='right corner' 
-        placeholder='Search...' 
-    />
-)
+import { connect } from 'react-redux';
+import {searchTerm} from '../bill/actions'
 
-export default SearchBar
+class SearchBar extends Component {
+
+        filterText = () => {
+            console.log(this.props.findBill, "HERE IS FILTER TEXT FUNCTION")
+            let filteredText = this.props.changing_upcoming_bill_data.filter((aBill) => (
+                aBill.description.toLowerCase().includes(this.props.text.toLowerCase()) ||
+                aBill.bill_number.toLowerCase().includes(this.props.text.toLowerCase())
+            )
+            )
+            return filteredText
+        }
+
+
+    render(){
+        return (
+            <Input 
+                value={this.props.text} 
+                onChange={(event)=> this.props.searchTerm(event, "FIRED --------FIRED")} 
+                findbill={this.props.findBill(this.filterText())}
+                action={{color: 'teal'}} label={{ icon: 'asterisk' }} 
+                labelPosition='right corner' 
+                placeholder='Search...' 
+            />
+            )
+    }
+}
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        text: state.getBillsReducer.searchTerm,
+        changing_upcoming_bill_data: state.getBillsReducer.changing_upcoming_bill_data,
+    }
+}
+
+export default connect(mapStateToProps, {searchTerm})(SearchBar)
