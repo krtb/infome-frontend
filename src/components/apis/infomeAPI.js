efaa07d98252ce53d221567b77676dd689a60684
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-12 11:47:29 -0500

    add axios headers to exios infome backend api object

--- src/components/apis/infomeAPI.js
+++ src/components/apis/infomeAPI.js
@@ -1,5 +1,10 @@
 import axios from 'axios'; 
 
-export default axios.create({
-    infomeRailsHerokuAPI: 'https://infome-backend.herokuapp.com/api/v1'
+const infomeInstance = axios.create({
+    baseURL: 'https://infome-backend.herokuapp.com/api/v1'
 });
+
+infomeInstance.defaults.headers.common['Content-Type'] = 'Application/json'
+infomeInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('jwt')}`;
+
+export default infomeInstance
