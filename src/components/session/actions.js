db40fc1552398fb62799fd87f26884c400d2a9f2
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-17 14:19:23 -0500

    reformat loginUser func with asyc/await, axios

--- src/components/session/actions.js
+++ src/components/session/actions.js
@@ -1,38 +1,30 @@
 import infoMeApi from '../apis/infomeAPI' // fetch from dev: 'http://localhost:3000/api/v1'
 import { AUTHENTICATING_USER, SET_CURRENT_USER } from './types'
 
-export const loginUser = (name, password) => {
-
-    return dispatch => {
-        dispatch(authenticatingUser())
-        const prod_login_endpoint = 'https://infome-backend.herokuapp.com/api/v1/login'
-        // TODO:const dev_endpoint= 'http://localhost:3001/api/v1/login' ---> remove dev api url, for prod '/resources' 
-        const postFetchObject = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({ user: { name, password } })
-        }
-
-        fetch(prod_login_endpoint, postFetchObject)
-            .then(response => response.json())
-            .then(({ user, jwt }) => {
-                localStorage.setItem('jwt', jwt)
-                dispatch(setCurrentUser(user))
-            })
-            .catch(error =>
-                console.log(error.message, 'ERROR HERE!') //fix catch syntax
-            )
-    }
-}
-
-export const authenticatingUser = () => (
-    { type: AUTHENTICATING_USER }
-)
+export const authenticatingUser = () => ({ 
+    type: AUTHENTICATING_USER 
+})
 
 export const setCurrentUser = userData => ({
     type: SET_CURRENT_USER,
     payload: userData
 })
+
+export const loginUser = (name, password) => async dispatch => {
+    try {
+        dispatch(authenticatingUser())
+
+        const data = { user: { name, password } }
+        const response = await infoMeApi.post('/login', data)
+
+        const userJWT = await response.data.jwt
+        const userData = await response.data.user
+        
+        localStorage.setItem('jwt', userJWT)
+        dispatch(setCurrentUser(userData))
+
+    } catch (error) {
+        console.log(error, "in session/actions/loginUser")
+    }
+    // async always returns a promise, https://scotch.io/tutorials/asynchronous-javascript-using-async-await
+}
