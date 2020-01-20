d8f40eba0446472fe6d3a2c916f28d5d63c0fe00
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-20 09:38:55 -0500

    make CreateAccountForm a controlled component

--- src/components/session/CreateAccountForm.js
+++ src/components/session/CreateAccountForm.js
@@ -1,29 +1,90 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Link } from "react-router-dom";
-import { Button, Form, Dropdown } from 'semantic-ui-react'
+import { Redirect } from 'react-router-dom';
+import * as actions from './actions'
+import { connect } from 'react-redux';
+
 import stateOptions from './statesDB'
+import { Button, Form, Dropdown } from 'semantic-ui-react'
 
 class CreateAccountForm extends Component {
+    state = {
+        username: '',
+        password: '',
+        email: '',
+        zip_code: '',
+        political_party: ''
+    }
 
+    handleChange = (e) => {        
+        const value = e.target.value
+        this.setState({
+            ...this.state,
+            [e.target.name]: value
+        })
+    }
+
+    handleLoginSubmit = () => {
+        // send user properties to redux action creator
+        this.props.createNewUser(
+            this.state.username, 
+            this.state.password, 
+        )
+        
+        // reset form to blanks
+        this.setState({ 
+            username: '', 
+            password: '',
+            email: '',
+            zip_code: '',
+            political_party: '' 
+        })
+    }
     
 
     render() {
-        return (
-            <Form>
+        return this.props.loggedIn ? (
+            <Redirect to="/savedbills" />
+        ) : (
+
+            <Form onSubmit={this.handleLoginSubmit}>
+
                 <Form.Group unstackable widths={2}>
-                    <Form.Input label='User Name' placeholder='User Name' />
-                    <Form.Input label='Password' placeholder='Password' />
+                    <Form.Input label='User Name' placeholder='User Name' 
+                        name="username"
+                        onChange={this.handleChange}
+                        value={this.state.username}
+                    />
+                    <Form.Input label='Password' placeholder='Password' 
+                        name="password"
+                        onChange={this.handleChange}
+                        value={this.state.password}
+                    />
                 </Form.Group>
+
                 <Form.Group widths={2}>
-                    <Form.Input label='Zip Code' placeholder='Zip Code' />
-                    <Form.Input label='Political Party' placeholder='Political Party' />
+                    <Form.Input label='Zip Code' placeholder='Zip Code' 
+                        name="zip_code"
+                        onChange={this.handleChange}
+                        value={this.state.zip_code}
+                    />
+                    <Form.Input label='Political Party' placeholder='Political Party' 
+                        name="political_party"
+                        onChange={this.handleChange}
+                        value={this.state.political_party}
+                    />
                 </Form.Group>
+
                 <Dropdown placeholder='State' search selection options={stateOptions} />
+
                 <Form.Checkbox label='I agree to the Terms and Conditions' />
-                <Link to="/searchbills"><Button type='submit'>Submit</Button></Link>
+
+                <Button type='submit'>Submit</Button>
+
             </Form>
+
         );
     }
 }
 
-export default CreateAccountForm;
+
+export default connect(null, actions)(CreateAccountForm);
