a6605be6b0072ade9811d4e53b5c7a7f5acfe5b6
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-22 14:34:04 -0500

    map redux state to SearchBills component props

--- src/components/bill/SearchBills.js
+++ src/components/bill/SearchBills.js
@@ -9,36 +9,54 @@ import * as actions from './actions'
 // const token = localStorage.getItem('jwt')
 
 class SearchBills extends Component {
-    // TODO: CHANGE TO USE STATE FROM BILLSREDUCER.JS
-    state = {
-        changing_upcoming_bill_data: [],
-    }
 
     componentDidMount() {
         this.props.fetchBills()
     }
 
-    // TODO: MOVE THIS TO ACTIONS FILE
-    findBill = (bill) => {
-            this.setState({ 
-                changing_upcoming_bill_data: bill 
-            })
+    onTermSubmit = async term => {
+        console.log(term);
+        this.setState({
+            changing_upcoming_bill_data: term,
+        })
     }
 
     render() {
-        console.log(this.state.changing_upcoming_bill_data, 'PROPS DATA')
 
         return (
 
             <React.Fragment>
+
             <MenuTabs/>
+
                 <div className="my-searchbar">
-                <SearchBar findBill={this.findBill} />
+
+                    <SearchBar onFormSubmit={this.onTermSubmit}/>
+
                 </div>
-                <BillsList handleBillChoiceClick={this.props.handleBillChoiceClick} isConcerning={this.props.isConcerning} isProductive={this.props.isProductive}  addNegaToUser={this.props.addNegaToUser} addToUser={this.props.addToUser} changeUpcomBilDat={this.state.changing_upcoming_bill_data} />
+
+                <BillsList 
+
+                    handleBillChoiceClick={this.props.handleBillChoiceClick}
+                    isConcerning={this.props.isConcerning} 
+                    isProductive={this.props.isProductive}  
+                    addNegaToUser={this.props.addNegaToUser} 
+                    addToUser={this.props.addToUser} 
+
+                    changeUpcomBilDat={this.props.changing_upcoming_bill_data} 
+                />
+
             </React.Fragment>
         );
     }
 }
 
-export default connect(null, actions)(SearchBills);
+function mapStateToProps (state) {
+    
+    return{
+        changing_upcoming_bill_data: state.billsReducer.changing_upcoming_bill_data,
+        upcoming_bill_data: state.billsReducer.upcoming_bill_data,
+    }
+}
+
+export default connect(mapStateToProps, actions)(SearchBills);
