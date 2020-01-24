db017b6c57b0af94c6624c6098701b7213b8bee9
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-24 14:06:42 -0500

    remove dropdown, display based on redux loggedIn

--- src/components/common/MenuTabs.js
+++ src/components/common/MenuTabs.js
@@ -1,23 +1,119 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { Button, Dropdown, Menu } from 'semantic-ui-react'
+import { logOutUser } from '../session/actions';
+import { Button, Dropdown, Menu } from 'semantic-ui-react';
 
-export default class MenuExampleSizeSmall extends Component {
+class MenuTabs extends Component {
     state = { activeItem: 'home' }
 
-    handleLogOut = () => {
-        localStorage.clear()
-    }
+    // handleLogOut = () => {
+    //     // TODO: set loggedIn state to false
+    //     this.setState({
+    //         loggedIn: false
+    //     })
+    //     localStorage.clear()
+    // }
 
+    renderContent = () => {
+        switch (this.props.loggedIn) {
+            case false:
+                return [
+
+                    
+                
+                    <Menu.Item
+                    as={Link}
+                    to='/CreateAccountForm'
+                    name='sign-up'
+                    active={this.state.activeItem === 'sign-up'}
+                    onClick={this.handleLogOut}
+                    key="1"
+                > 
+                    <Button secondary fluid>
+                        Sign Up
+                    </Button>
+                </Menu.Item> ,
+
+                    <Menu.Item
+                        as={Link}
+                        to="/login"
+                        name='log-out'
+                        active={this.state.activeItem === 'log-in'}
+                        onClick={this.props.logOutUser}
+                        key="2"
+                    >
+                        <Button primary>Log In
+                </Button>
+                    </Menu.Item>
+                    ]
+
+            default:
+                return [
+                
+                    <Menu.Item
+                        as={Link}
+                        to='/searchbills'
+                        name='Search Bills'
+                        active={this.state.activeItem === 'Search Bills'}
+                        onClick={this.handleItemClick}
+                        key="1"
+
+                    >
+                        Search Bills
+                            </Menu.Item>,
+
+                    <Menu.Item
+                        as={Link}
+                        to='/savedbills'
+                        name='My Saved Bills'
+                        active={this.state.activeItem === 'My Saved Bills'}
+                        onClick={this.handleItemClick}
+                        key="2"
+
+                    >
+                        Saved Bills
+                            </Menu.Item>,
+
+                    <Menu.Item
+                        as={Link}
+                        to='/myprofile'
+                        name='my-profile'
+                        active={this.state.activeItem === 'my-profile'}
+                        onClick={this.handleItemClick}
+                        key="3"
+
+                    >
+                        My Profile
+                            </Menu.Item>,
+
+                <Menu.Item
+                    as={Link}
+                    to='/'
+                    name='log-out'
+                    active={this.state.activeItem === 'log-out'}
+                    onClick={this.props.logOutUser}
+                    key="4"
+
+                >
+                <Button primary>Log Out
+                </Button>
+                </Menu.Item>]
+                
+                
+        }
+    }
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
     render() {
+
+        
         const { activeItem } = this.state
 
         return (
-            <Menu size='small'>
+            <Menu size='small' style={{ position: 'relative' }}>
                 <Menu.Item
                     name='InfoMe'
                     as={Link}
@@ -28,52 +124,22 @@ export default class MenuExampleSizeSmall extends Component {
 
 
                 <Menu.Menu position='right'>
-                    <Dropdown item text='Options'>
-                        <Dropdown.Menu>
-                            <Dropdown.Item
-                                as={Link}
-                                to='/searchbills'
-                                name='Search Bills'
-                                active={activeItem === 'Search Bills'}
-                                onClick={this.handleItemClick}
-                            >
-                                Search Bills
-                            </Dropdown.Item>
+                    
 
-                            <Dropdown.Item
-                                as={Link}
-                                to='/savedbills'
-                                name='My Saved Bills'
-                                active={activeItem === 'My Saved Bills'}
-                                onClick={this.handleItemClick}
-                            > 
-                                Saved Bills
-                            </Dropdown.Item>
 
-                            <Dropdown.Item
-                                as={Link}
-                                to='/myprofile'
-                                name='my-profile'
-                                active={activeItem === 'my-profile'}
-                                onClick={this.handleItemClick}
-                            >
-                                My Profile
-                            </Dropdown.Item>
+                        {this.renderContent()}
 
-                        </Dropdown.Menu>
-                    </Dropdown>
-
-                    <Menu.Item
-                        as={Link}
-                        to='/'
-                        name='log-out'
-                        active={activeItem === 'log-out'}
-                        onClick={this.handleLogOut}
-                    >
-                        <Button primary>Log Out</Button>
-                    </Menu.Item>
                 </Menu.Menu>
             </Menu>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.user,
+        loggedIn: state.userReducer.loggedIn
+    }
+}
+
+export default connect(mapStateToProps, { logOutUser})(MenuTabs);
