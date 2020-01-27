32d68279f96c39e54a8c4143b7260fa82701ec2d
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-26 20:58:58 -0500

    remove unused header element from semanticUI

--- src/components/common/NotFound.js
+++ src/components/common/NotFound.js
@@ -1,13 +1,10 @@
 import React, { Fragment } from 'react'
-import { Header, Image } from 'semantic-ui-react'
+import { Image } from 'semantic-ui-react'
 
 import NotFoundImg from '../../static/images/404page.png'
 
 const NotFound = () => (
     <Fragment>
-        {/* <Header size="huge" inverted color="purple">
-            Page Not Found lol
-        </Header> */}
         <Image src={NotFoundImg} />
     </Fragment>
 )
