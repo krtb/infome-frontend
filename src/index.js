e3fa63dfdd458b8eece397a1514fb160c340902a
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-28 15:44:15 -0500

    persist state in App component on load

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// REDUX
+import * as reducers from './components';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux'
+// ROUTER
 import { BrowserRouter as Router } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
-
+// APP
 import App from './App';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
-import configureStore from './store/configureStore';
-const store = configureStore()
+
+
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (e) {
+        return undefined;
+    }
+};
+
+const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch (e) {
+        console.log(e, 'error in index.js')
+    }
+};
+
+const rootReducer = combineReducers(reducers);
+const peristedState = loadState();
+
+const store = createStore(
+    rootReducer,
+    peristedState,
+    composeWithDevTools(applyMiddleware(thunk)
+    ));
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 ReactDOM.render(
     <Provider store={store}>
