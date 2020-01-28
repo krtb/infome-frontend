357a618b88d334a2bfac4ee90d2a399ff15a1dbf
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-28 17:04:48 -0500

    move import of UserProfile for clearer reading

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import MenuTabs from './components/common/MenuTabs';
 import LandingPage from './components/common/LandingPage';
 import NotFound from './components/common/NotFound';
 // SESSION
+import UserProfile from './components/session/UserProfile';
 import CreateAccountForm from './components/session/CreateAccountForm';
 import LoginForm from './components/session/LoginForm';
 // BILLS
 import SearchBills from './components/bills/SearchBills';
 import SavedBillsTable from './components/bills/SavedBillsTable';
-import UserProfile from './components/session/UserProfile';
 
 class App extends Component {
 
   render() {
     return (
 
-      <BrowserRouter>
-
+        <BrowserRouter>
+        
         <MenuTabs />
 
         <Switch>
