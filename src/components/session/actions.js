aa558a6af674d4f1e75800c2137408a708959c81
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-28 16:58:25 -0500

    add redux action creators to edit user profile

--- src/components/session/actions.js
+++ src/components/session/actions.js
@@ -1,5 +1,13 @@
 import infoMeApi from '../apis/infomeAPI'
-import { AUTHENTICATING_USER, SET_CURRENT_USER, LOG_OUT_USER } from './types'
+
+import { 
+    AUTHENTICATING_USER, 
+    SET_CURRENT_USER, 
+    LOG_OUT_USER, 
+    PROFILE_CHANGE,
+    REQUEST_EDIT,
+    DISCARD_EDIT
+} from './types'
 
 export const authenticatingUser = () => ({ 
     type: AUTHENTICATING_USER 
@@ -11,11 +19,11 @@ export const setCurrentUser = userData => ({
 })
 
 export const logOutUser = () => async dispatch => {
-    
     localStorage.clear()
     dispatch({ type: LOG_OUT_USER })
 }
 
+// (API): LOG IN EXISTING USER
 export const loginUser = (name, password) => async dispatch => {
     
     try {
@@ -24,7 +32,7 @@ export const loginUser = (name, password) => async dispatch => {
         const data = { user: { name, password } }
 
         const response = await infoMeApi.post('/login', data)
-        
+        const { jwt } = await response.data
         const userJWT = await response.data.jwt
         const userData = await response.data.user
 
@@ -38,7 +46,7 @@ export const loginUser = (name, password) => async dispatch => {
     }
 }
 
-
+// (API): CREATE NEW USER
 export const createNewUser = (name, password) => async dispatch => {
     try {
         let userObject = {
@@ -58,6 +66,62 @@ export const createNewUser = (name, password) => async dispatch => {
         infoMeApi.post('/users', userObject, axiosConfig)
 
     } catch (error) {
-        console.log(error, "in createNewUser(), session/actions/loginUser")
+        console.log(error, "in createNewUser(), session/actions")
+    }
+}
+
+// REQUEST PROFILE CHANGES || DISCARD CHANGES
+export const onEditClick = (requestEdit, event, userCopy) => async dispatch => {
+
+    if (event.target.name === 'discard'){
+        dispatch({ type: DISCARD_EDIT, payload: !requestEdit })
+    } 
+
+    dispatch({ type: REQUEST_EDIT, payload: !requestEdit })
+}
+
+// SEND UPDATES TO REDUX STATE && EDIT USER PROFILE
+export const updateUserInfo = (event, userCopy) => async dispatch => {  
+    event.preventDefault()
+
+    const name = event.target.name;
+    const value = event.target.value;
+
+    const newUserValues = {
+        ...userCopy,
+        [name]: value
+    };
+
+    dispatch({ type: PROFILE_CHANGE, payload: newUserValues })
+    
+}
+
+// (API): POST USER PROFILE CHANGES TO BACKEND API ON SAVE CLICK
+export const updateUserProfile = (event, userCopy) => async dispatch => {
+    event.preventDefault()
+
+    try {
+
+        let userObject = {
+            "user": {
+                "name": userCopy.name,
+            }
+        }
+
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*",
+            }
+        };
+        
+        let postingUser = await infoMeApi.put(`/users/${userCopy.id}`, userObject, axiosConfig)
+        // TODO: pull in new user profile info
+        // TODO: display success
+        // TODO: route back to edit page
+        console.log(postingUser, 'in updateUserProfile(), session/actions')
+        
+    } catch (error) {
+        console.log(error, "updateUserProfile(), session/actions")
     }
 }
