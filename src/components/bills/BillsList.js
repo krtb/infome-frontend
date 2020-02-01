1774f04ebd9c6da79e1c73836b3a61562df21150
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-31 19:04:08 -0500

    add pagination and associated state, functions

--- src/components/bills/BillsList.js
+++ src/components/bills/BillsList.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
-import Bill from './Bill'
-import { Table,
-    Grid,
-    Segment,
-    Container,
-    Header,
-    Pagination,
-    PaginationProps,
-} from 'semantic-ui-react'
-
+import React from 'react';
 import { connect } from 'react-redux';
-import {searchTerm} from '../bills/actions'
+import { searchTerm, fetchBills, setPageNumInState } from '../bills/actions'
 
+// Components & Styling
+import Bill from './Bill'
+import { Table, Pagination } from 'semantic-ui-react'
 
 const BillsList = (props) => {    
 
+    let totalPages = (props.alteredBillList.length / props.itemsPerPage).toFixed()
+    
+    let setPageNum = (event, { activePage }) => {
+        props.setPageNumInState(activePage);
+    };
+
+    let items = props.alteredBillList.slice(
+        (props.page - 1) * props.itemsPerPage,
+        (props.page - 1) * props.itemsPerPage + props.itemsPerPage
+    )
+
+    console.log(items, 'items', props.page, 'page', props.itemsPerPage, 'items per page')
+    
     let renderDataNow = () => {
-        return props.alteredBillList.map((one) =>
-            <Bill id={one.id} key={one.bill_id} one={one}/>
+        return items.map((one) =>
+            <Bill id={one.id} key={one.id} one={one}/>
         )
     }
-
+    console.log(props.alteredBillList, 'altered bill list')
     return (
         <React.Fragment>
             <Table celled>
@@ -39,20 +45,28 @@ const BillsList = (props) => {
                     {renderDataNow()}
                 </Table.Body>
             </Table>
-            {/* TODO: connect pagination callback to backend pages */}
-            {/* <Pagination
-                activePage={activePage}                
-                // totalPages={Math.ceil(props.alteredBillList.length / 5)}
-                onPageChange={onChange}            /> */}
+
+            <Pagination
+                activePage={props.page}
+                totalPages={totalPages}
+                siblingRange={1}
+                onPageChange={setPageNum}        
+            />
 
         </React.Fragment>
     );
 }
 
 const mapStateToProps = (state) => {
+    console.log(state, 'bills list state');
+    
     return {
+        initialBillList: state.billsReducer.initialBillList,
         alteredBillList: state.billsReducer.alteredBillList,
+        page: state.billsReducer.page,
+        itemsPerPage: state.billsReducer.itemsPerPage,
+        searching: state.billsReducer.searching
     }
 }
 
-export default connect(mapStateToProps, {searchTerm})(BillsList);
+export default connect(mapStateToProps, { searchTerm, fetchBills, setPageNumInState})(BillsList);
