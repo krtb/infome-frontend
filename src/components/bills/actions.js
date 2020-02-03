0799b2fa8ee7370a5c8c1a80a3dafc5e0148d755
Kurt Bauer <kurtbauer55@protonmail.com>
2020-02-03 11:15:45 -0500

    add callback to removeNullValues when fetching

--- src/components/bills/actions.js
+++ src/components/bills/actions.js
@@ -7,54 +7,60 @@ import {
     CHOSE_PRODUCTIVE_BILL, 
     CHOSE_CONCERNING_BILL, 
     DELETE_PRODUCTIVE_BILL,
-    DELETE_CONCERNING_BILL
+    DELETE_CONCERNING_BILL,
+    PAGE_CHANGE
     } from './types'
+
+// (used in): fetchBills() below
+export const removeNullValues = (listOfBills) => {
+
+    let nullValuesRemoved = listOfBills.filter((value) => {
+        return value.description !== null;
+    })
+    return nullValuesRemoved
+}
 
 // (used in): SearchBills
 export const fetchBills = () => async dispatch => {
-    const response = await getAllBills.get('/fetchbills');
-    const listOfBills = await response.data.results[0].bills
-    
-    dispatch({ type: FETCH_BILLS, payload: listOfBills})
+
+    // changed from .get(fetchbills)
+    const response = await getAllBills.get('/bills');        
+    const listOfBills = await response.data.bills
+
+    let nullValuesRemoved = removeNullValues(listOfBills)    
+
+    dispatch({ type: FETCH_BILLS, payload: nullValuesRemoved})
 };
 
-// (used in): SearchBar
+// (used in): SearchBarsrc/components/bills/billsReducer.js
 export const searchTerm = searchTerm => dispatch => {
     
     searchTerm.preventDefault()
 
-    dispatch({
-        type: SEARCH,
-        payload: searchTerm.target.value
-    });
+    dispatch({type: SEARCH, payload: searchTerm.target.value});
 };
 
 // (used in): SearchBar
-export const filterText = (searchTerm, props) => async dispatch => {
+export const filterText = (searchTerm, initialBillList) => async dispatch => {
 
-    let filteredText = props.initialBillList.filter((aBill) => (
-
+    let filteredText = initialBillList.filter((aBill) => (
         aBill.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
         aBill.bill_number.toLowerCase().includes(searchTerm.toLowerCase())
-
-      )
+        )
     )    
 
     let foundBill = await filteredText
 
     dispatch({ type: FIND_BILL, payload: foundBill })
-
 }
 
-// TODO: (used in): SavedBillsTable && SearchBills
+// (used in): SavedBillsTable && SearchBills, POST to Bills in RAILS API
 export const fetchPostBill = (bill) => {
-    console.log('FETCH POST BILL FUNCTION');
-
+    // TODO: refactor to use Async/Await
     //TODO: add new Bill class attributes, after backend API migration
     let user_data = bill
     JSON.stringify(user_data)
 
-    // POSTING TO BILLS
     let dev_api = `${process.env.REACT_APP_BACKEND_DEV_API}/picked_bills`
     let prod_api = `${process.env.REACT_APP_BACKEND_PROD_API}/picked_bills`
 
@@ -91,7 +97,6 @@ export const handleBillChoiceClick = (pickedBill, choice, billList) => async dis
 
 // DELETE a bill
 export const deleteBill = (listType, billId, props) => async dispatch => {
-    console.log(props.list, ' LIST TYPE ')
 
     let filteredList = listType.listType.filter((singleBill)=> {
         return singleBill.bill_id !== billId
@@ -105,4 +110,9 @@ export const deleteBill = (listType, billId, props) => async dispatch => {
     dispatch({ type: DELETE_CONCERNING_BILL, payload: filteredList })
     }
     
+}
+
+// (used in): BillsList
+export const setPageNumInState = (pageNumber) => async dispatch => {
+    dispatch({ type: PAGE_CHANGE, payload: pageNumber  })
 }
