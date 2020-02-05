f45d0d84be112ed14abca3165bc833a24050d12f
Kurt Bauer <kurtbauer55@protonmail.com>
2020-02-05 14:35:57 -0500

    create place holder component for SavedBillsTable

---
+++ src/components/common/SavedPlaceHolder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Table } from 'semantic-ui-react';
+
+const SavedPlaceHolder = () => {
+    return (
+    
+        <Table.Row>
+            <Table.Cell> PENDING SAVED</Table.Cell>
+        </Table.Row>
+
+    )
+}
+
+export default SavedPlaceHolder;
